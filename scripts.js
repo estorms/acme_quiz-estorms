4aa8296700fbd2b85fb4d7247f884943fcfcf602
Elizabeth Sanger <libsang@gmail.com>
2016-08-04 15:29:53 -0500

    pushing up before trying totally different approach

--- scripts.js
+++ scripts.js
@@ -1,37 +1,61 @@
-var Factory = (function (){
+var Factory = (function () {
 
-Promise.all([
-  $.getJSON('categories.json').then(function(data) {
-    categories = data.categories;
-    //console.log(categories);
-    categories.forEach(function (item) {
-    categories = item
-    console.log(categories);
-    return categories
-    })
-}),
+    var categories = [];
+    var types = [];
+    var products = [];
 
-  $.getJSON('types.json').then(function(data) {
-    types = data.types;
-    types.forEach(function(item){
-    types = item
-    console.log(types)
+    return {
+        getJSON : function () {
+            return Promise.all([
+                $.getJSON('categories.json')
+                    .then(function(data) {
+                        categories = data.categories;
+                        //console.log(categories);
+                        categories.forEach(function (item) {
+                            categories = item;
+                            // console.log(categories);
+                        })
 
-    return types;
-    })
-}),
+                        console.log(categories)
+                        return categories;
 
-  $.getJSON('products.json').then(function(data) {
-    products = data.products;
-    products.forEach(function(item){
-    products = item
-    console.log(products)
-    return products;
-    })
+                    }),
 
-})
-])
+               $.getJSON('types.json')
+                    .then(function(data) {
+                        types = data.types;
+                        types.forEach(function(item){
+                            types = item;
+                            // console.log(types)
+                        })
+
+                        return types;
+                    }),
+
+                $.getJSON('products.json')
+                    .then(function(data) {
+                        products = data.products;
+                        products.forEach(function(item){
+                            products = item;
+                            // console.log(products)
+                        })
+
+                        return products;
+                    })
+            ])
+        },
+        getCategories: function () {
+            return categories;
+        }
+    }
 })()
+
+Factory.getJSON().then(function (data) {
+    console.log(data)
+    console.log(Factory.getCategories())
+})
+
+
 
 
 
