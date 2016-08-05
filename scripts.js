e23ac994882af1657cf51e3e79d6f6eee0e6e905
Elizabeth Sanger <libsang@gmail.com>
2016-08-05 09:42:47 -0500

    global variable established

--- scripts.js
+++ scripts.js
@@ -1,26 +1,38 @@
-var Factory = (function () {
-var store = {};
-    return {
+var Factory = (function (fact) {    //define global variable (Factory), pass in argument of 'fact' to act as object upon which to build methods within the global object ('fact is internal', Factory is global)
+var store = {}; //declare empty object to hold JSON data
 
-    getStore: function (){
-        return store;
-    },
-
-    loadProducts: function (){
+    fact.loadProducts = function (){ // load products with $getJSON & Promises, define product load as a  method on internal object
         Promise.all ([
         $.getJSON('categories.json'),
         $.getJSON('types.json'),
         $.getJSON('products.json')
         ])
 
-        .then(function (catalog) {
-        store.categories = catalog[0].categories
+    .then(function (catalog) {
+    store.categories = catalog[0].categories
         store.types = catalog[1].types
         store.products = catalog[2].products[0]
-         })
-        }
-    }
-})()
+    }) //retrieve product data, define as store object key/value pairs
+}
 
-var acme = Factory.getStore();
-console.log(acme)
+  fact.getCategories = function (){
+        return store.categories //allow method of object retrieval via global variable
+    }
+
+    fact.getTypes = function () { //allow method of object retrieval via global variable
+
+        return store.types
+    }
+
+  fact.getProducts = function () {
+        return store.products //allow method of object retrieval via global variable
+    }
+
+
+    return fact //return local object built within global variable
+
+})(Factory || {}) // had to add empty object as alternative load
+
+
+
+
