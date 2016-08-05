89d68d987a167c830c0664619878e97b13fd9dd8
Elizabeth Sanger <libsang@gmail.com>
2016-08-04 20:42:44 -0500

    refactoring initial IIFE

--- scripts.js
+++ scripts.js
@@ -1,62 +1,26 @@
 var Factory = (function () {
-
-    var categories = [];
-    var types = [];
-    var products = [];
-
+var store = {};
     return {
-        getJSON : function () {
-            return Promise.all([
-                $.getJSON('categories.json')
-                    .then(function(data) {
-                        categories = data.categories;
-                        //console.log(categories);
-                        categories.forEach(function (item) {
-                            categories = item;
-                            // console.log(categories);
-                        })
 
-                        console.log(categories)
-                        return categories;
+    getStore: function (){
+        return store;
+    },
 
-                    }),
+    loadProducts: function (){
+        Promise.all ([
+        $.getJSON('categories.json'),
+        $.getJSON('types.json'),
+        $.getJSON('products.json')
+        ])
 
-               $.getJSON('types.json')
-                    .then(function(data) {
-                        types = data.types;
-                        types.forEach(function(item){
-                            types = item;
-                            // console.log(types)
-                        })
-
-                        return types;
-                    }),
-
-                $.getJSON('products.json')
-                    .then(function(data) {
-                        products = data.products;
-                        products.forEach(function(item){
-                            products = item;
-                            // console.log(products)
-                        })
-
-                        return products;
-                    })
-            ])
-        },
-        getCategories: function () {
-            return categories;
+        .then(function (catalog) {
+        store.categories = catalog[0].categories
+        store.types = catalog[1].types
+        store.products = catalog[2].products[0]
+         })
         }
     }
 })()
 
-Factory.getJSON().then(function (data) {
-    console.log(data)
-    console.log(Factory.getCategories())
-})
-
-
-
-
-
-
+var acme = Factory.getStore();
+console.log(acme)
