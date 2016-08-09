3cbb16b2ead7ff43e17fb7ab76e7c9391e1325cd
Elizabeth Sanger <libsang@gmail.com>
2016-08-09 14:48:51 -0500

    pushing before function overhaul

--- DOMload.js
+++ DOMload.js
@@ -1,17 +1,12 @@
 $(document).ready();
 
-
-
 var fireworks = [];
 var demolitions = [];
 
-// var dropdown = document.getElementById('dropdown');
-// dropdown.addEventListener('click', function (e){
-// document.getElementById('output').innerText = ''
 
-// })
 
 function putThingsInDom(categories,types,products){
+
 
 for (var i in products) {
     if (products[i].type <= 2) {
@@ -22,41 +17,48 @@ for (var i in products) {
     }
 }
 
-demolitions.forEach(function (demo) {
 
-
-   $( "#destroyThePlanet" ).click(function() {
-        $("#output").toggleClass('hidden').append(`<div class = "col-md-3 card">
-
-            <h2>Category: ${categories[types[demo.type].category].name}</h2>
-            <h3>Type: ${types[demo.type].name}</h3>
-            <h4>Product Name: ${demo.name}</h4>
-            ${demo.description}</div>`)
-        });
-
-})
 
 fireworks.forEach(function (firework){
 
 
         $( "#familyFun" ).click(function() {
+            $("#output").append(`
 
-             $("#output").toggleClass('hidden').append(`<div class = "col-md-4 card">
+                <div class="col-md-3 card">
             <h2>Category: ${categories[types[firework.type].category].name}</h2>
             <h3>Type: ${types[firework.type].name}</h3>
-            <h4>Product Name: ${firework.name}</h4>
-            ${firework.description}</div>`)
-
-
+            <h5>Product Name: ${firework.name}</h5>
+            <p>${firework.description}</p></div>
+            <div class="col-md-1"></div>
+            `)
 
         });
 
 })
 
+$("#output").html("")
+demolitions.forEach(function (demo) {
 
 
+   $( "#destroyThePlanet" ).click(function() {
+        $("#output").append(`<div class = "col-md-3 card">
+        <h2>Category: ${categories[types[demo.type].category].name}</h2>
+            <h3>Type: ${types[demo.type].name}</h3>
+            <h5>Product Name: ${demo.name}</h5>
+            <p>${demo.description}</p></div>
+            <div class="col-md-1"></div>`)
+        });
+
+})
 
 
 }
+
+
+//click, calls function, function clears, function runs for each
+
+
+
 
 Factory.loadProducts(putThingsInDom);
