9de9fd250ecfa85e649083b88743de5542f172a7
Elizabeth Sanger <libsang@gmail.com>
2016-08-09 15:42:24 -0500

    cleaning up styling

--- DOMload.js
+++ DOMload.js
@@ -23,9 +23,9 @@ function fireworksPop () {
     $("#output").empty();
     fireworks.forEach(function (firework){
         $("#output").append(`
-        <div class="col-md-3 card">
-        <h2>Category: ${categories[types[firework.type].category].name}</h2>
-        <h3>Type: ${types[firework.type].name}</h3>
+        <div class="col-md-3 card" style="background-color: rgba(255, 0, 0, 0.8); color: black; font-style: bolder">
+        <h3>Category: ${categories[types[firework.type].category].name}</h3>
+        <h4>Type: ${types[firework.type].name}</h4>
         <h5>Product Name: ${firework.name}</h5>
         <p>${firework.description}</p></div>
         <div class="col-md-1"></div>`)});
@@ -36,21 +36,16 @@ function demoPop () {
     $("#output").empty();
     demolitions.forEach(function (demo){
         $("#output").append(`
-        <div class="col-md-3 card">
-        <h2>Category: ${categories[types[demo.type].category].name}</h2>
-        <h3>Type: ${types[demo.type].name}</h3>
+        <div class="col-md-3 card" style="background-color: rgba(255, 0, 0, 0.8); color: black; font-style: bolder">
+        <h3>Category: ${categories[types[demo.type].category].name}</h3>
+        <h4>Type: ${types[demo.type].name}</h4>
         <h5>Product Name: ${demo.name}</h5>
         <p>${demo.description}</p></div>
         <div class="col-md-1"></div>`)});
 
 }
 
-
 }
-
-
-//click, calls function, function clears, function runs for each
-
 
 
 
